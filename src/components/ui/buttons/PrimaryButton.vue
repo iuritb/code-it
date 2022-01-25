56676f6033f9513f9289f5de4eb3c5258ee3de25
iuri.barros <iuri.t.barros@gmail.com>
2022-01-25 19:45:34 -0300

    create buttons

---
+++ src/components/ui/buttons/PrimaryButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <button>{{ label }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["label"],
+};
+</script>
--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
     <h1>tela de cursos</h1>
+    <PrimaryButton :label="label" />
   </div>
 </template>
+
+<script>
+//UI Components
+import PrimaryButton from "../components/ui/buttons/PrimaryButton.vue";
+export default {
+  components: {
+    PrimaryButton
+  },
+  data() {
+    return {
+      label: 'teste'
+    }
+  }
+};
+</script>
