52df2f75dab4f86a5b049450f7d8df43331d9534
iuri.barros <iuri.t.barros@gmail.com>
2022-01-25 14:50:35 -0300

    update breadcrumb

--- src/components/layout/BreadCrumb.vue
+++ src/components/layout/BreadCrumb.vue
@@ -1,5 +1,11 @@
 <template>
-  <nav class="container"></nav>
+  <nav class="breadcrumb">
+    <div class="breadcrumb__content">
+      <span class="breadcrumb__content-item">Home</span>
+      <span class="breadcrumb__content-item"> > </span>
+      <span class="breadcrumb__content-item">Treinamento</span>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -7,10 +13,20 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-.container {
+.breadcrumb {
   background-color: #dee2e6;
   display: flex;
+  justify-content: flex-start;
+  align-items: center;
   height: 3.12rem;
+  &__content {
+    color: #333333;
+    margin: 1.5rem 0.5rem 1.5rem 0.5rem;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 22px;
+    cursor: pointer;
+  }
 }
 </style>
 
