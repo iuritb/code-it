b15e91be23a97ae6a6ac04e9bbc9b5aabc7cf978
iuri.barros <iuri.t.barros@gmail.com>
2022-01-25 21:04:36 -0300

    update courses with course card

--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -7,6 +7,15 @@
         @click="newCourse('classes')"
         :label="'Novo Treinamento'"
       />
+      <template v-for="(course, index) in courses" :key="index">
+        <CardCourse
+          class="course__content-card"
+          :logo="course.logo"
+          :title="course.description"
+          :description="course.description"
+          :isActive="course.isActive"
+        />
+      </template>
     </div>
   </div>
 </template>
@@ -14,12 +23,28 @@
 <script>
 //UI Components
 import PrimaryButton from "../components/ui/buttons/PrimaryButton.vue";
+import CardCourse from "../components/ui/cards/CardCourse.vue";
 export default {
   components: {
     PrimaryButton,
+    CardCourse,
   },
   data() {
     return {
+      courses: [
+        {
+          title: "Curso de Svelte",
+          description: "Um curso para introdução ao Svelte com ...",
+          logo: "svelte.svg",
+          isActive: true,
+        },
+        {
+          title: "Curso de React",
+          description: "Como Criar aplicativos utilizando React ...",
+          logo: "react.svg",
+          isActive: false,
+        },
+      ],
       label: "teste",
     };
   },
@@ -50,8 +75,10 @@ export default {
     line-height: 30px;
   }
   &__content-button {
-
     align-self: flex-end;
+  }
+  &__content-card {
+    margin: 2rem 0 1rem;
   }
 }
 </style>
