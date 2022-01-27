44dce0368f3ab5943a56c2bea8ba77c57d736aa2
iuri.barros <iuri.t.barros@gmail.com>
2022-01-27 11:58:47 -0300

    Create tab component

---
+++ src/components/ui/Tab.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="tab">
+    <div class="tab_button">
+      <button class="tab__button-module" >Módulos</button>
+      <button class="tab__button-class">Aulas</button>
+    </div>
+    <div class="tab__divider"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.tab {
+  
+  &__button {
+    display: flex;
+    flex-direction: row;
+
+  }
+  &__button-module {
+    background: #1dbad3;
+    width: 200px;
+    height: 40px;
+    border: none;
+    text-transform: uppercase;
+    border-radius: 10px 0px 0px 0px;
+    cursor: pointer;
+  }
+  &__button-class {
+    background: #dee2e6;
+    width: 200px;
+    height: 40px;
+    border: none;
+    text-transform: uppercase;
+    border-radius: 0px 10px 0px 0px;
+    cursor: pointer;
+  }
+  &__divider {
+    background: #1dbad3;
+    width: 100%;
+    height: 2px;
+  }
+}
+</style>
