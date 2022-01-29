c19ad06d3be1d70b7d7bf8757dc431855e1bfe02
iuri.barros <iuri.t.barros@gmail.com>
2022-01-29 10:23:24 -0300

    Create danger & success button

--- src/components/ui/buttons/DangerButton.vue
+++ src/components/ui/buttons/DangerButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button>{{ label }}</button>
+    <button class="success-button">{{ label }}</button>
   </div>
 </template>
 
@@ -9,3 +9,23 @@ export default {
   props: ["label"],
 };
 </script>
+
+<style lang="scss" scoped>
+.success-button {
+  padding: 0.5rem 5rem 0.5rem 5rem;
+  background-color: #ec3f5f;
+  color: #fff;
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 1.5;
+  width: 150px;
+  height: 35px;
+  border-radius: 5px;
+  border: none;
+  text-transform: uppercase;
+  cursor: pointer;
+}
+.success-button:hover {
+  box-shadow: 0px 1px 10px 2px rgba(0.15, 0.15, 0.15, 0.3);
+}
+</style>
