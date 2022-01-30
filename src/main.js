2e688d7a379d3fc3b7eccb671d348fd6b081431e
iuri.barros <iuri.t.barros@gmail.com>
2022-01-30 13:24:26 -0300

    create vuex configuration

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
+import store from './store/store.js'
 import App from './App.vue'
 import router from './router/index.js'
 
 
-createApp(App).use(router).mount('#app')
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .mount('#app')
